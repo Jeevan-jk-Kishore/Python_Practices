f50e511b0eecff4b1f86a87966d4c8a5fbaefa7f
Jeevan Kishore <jeevanjk8888@gmail.com>
2025-12-19 20:59:15 +0530

    Polymorphism File Updated

---
+++ polymorphism.py
@@ -0,0 +1,13 @@
+#Polymorphism
+"""
+The same function name is used for different types 
+"""
+
+#Example 1
+def add(a,b):
+    print(a+b)
+def add(a,b,c):
+    print(a+b+c)
+
+add(10,20)
+add(10,20,30)
